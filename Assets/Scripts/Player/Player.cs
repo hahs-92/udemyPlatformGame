64601916f0e17d917ea54fc3ac4c10fbf96f657f
unknown <alexandre_2420@outlook.com>
2023-06-25 11:12:09 -0500

    section 3: 13. Collision detection

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -12,15 +12,45 @@ public class Player : MonoBehaviour
     public float jumpForce;
 
     private float movingInput;
+    public LayerMask whatIsGround;
+    public float groundCheckDistance;
+    public bool isGronded;
 
     private void Update()
     {
         movingInput = Input.GetAxis("Horizontal");
-        if(Input.GetKeyDown(KeyCode.Space))
+        CollisionChecks();
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            if(isGronded)
+            {
+                Jump();
+            }
         }
 
+        Move();
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void Move()
+    {
         rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
+    }
+
+    private void CollisionChecks()
+    {
+        isGronded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.
+            DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
