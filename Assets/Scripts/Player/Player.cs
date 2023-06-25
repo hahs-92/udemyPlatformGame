d18a959d872fddc25576a3ff3259664dfb47d387
unknown <alexandre_2420@outlook.com>
2023-06-25 10:13:07 -0500

    section 3: 10. Make character Jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move")]
     public float moveSpeed;
     public Rigidbody2D rb;
+
+    [Header("Jump")]
+    public float jumpForce;
 
     private float movingInput;
 
     private void Update()
     {
         movingInput = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
         rb.velocity = new Vector2(moveSpeed * movingInput, rb.velocity.y);
     }
 }
