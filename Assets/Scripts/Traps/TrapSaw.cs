8a0eaee1cbe29b1a9686e1d8c1fa42e7331893a2
unknown <alexandre_2420@outlook.com>
2023-07-01 15:43:27 -0500

    section 5: Trap Saw

--- Assets/Scripts/Traps/TrapSaw.cs
+++ Assets/Scripts/Traps/TrapSaw.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class TrapSaw : Trap
 {
     private Animator anim;
-    [SerializeField] private bool isWorking;
     [SerializeField] private Transform[] movePoints;
+    [SerializeField] private float speed;
+    [SerializeField] private float coolDown;
+
+    private bool isWorking;
+    private int movePointIndex;
+    private float coolDownTimer;
 
 
     private void Awake()
@@ -17,5 +22,32 @@ public class TrapSaw : Trap
     private void Update()
     {
         anim.SetBool("isWorking", isWorking);
+        isWorking = coolDownTimer < 0;
+        coolDownTimer -= Time.deltaTime;
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!isWorking) return;
+
+        transform.position = 
+            Vector3.MoveTowards(transform.position, movePoints[movePointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, movePoints[movePointIndex].position) < 0.15f) {
+            movePointIndex++;
+            coolDownTimer = coolDown;
+            Flip();
+
+            if(movePointIndex >= movePoints.Length)
+            {
+                movePointIndex = 0;
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        transform.localScale = new Vector3(1, transform.localScale.y * -1);
     }
 }
