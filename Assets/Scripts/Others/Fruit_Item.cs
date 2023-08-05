d9aa62bb11536d08ba8951cb630e4e97080efc73
unknown <alexandre_2420@outlook.com>
2023-08-05 10:01:28 -0500

    section 11: 76. Skins for fruits

--- Assets/Scripts/Others/Fruit_Item.cs
+++ Assets/Scripts/Others/Fruit_Item.cs
@@ -2,8 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum FruitType
+{
+    Apple,
+    Banana, 
+    Cherry,
+    Kiwi,
+    Melon,
+    Orange,
+    Pineapple,
+    Strawberry
+}
+
 public class Fruit_Item : MonoBehaviour
 {
+    private Animator anim;
+    private SpriteRenderer sr;
+    [SerializeField] private Sprite[] fruitImage;
+    public FruitType fruitType;
+
+    private void Awake()
+    {
+        anim= GetComponent<Animator>();
+        sr= GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetImage();
+        SetLayerWeight();
+    }
+
+    private void SetLayerWeight()
+    {
+        for (int i = 0; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+
+        anim.SetLayerWeight((int)fruitType, 1);
+    }
+
+    private void SetImage()
+    {
+        sr.sprite = fruitImage[(int)fruitType];
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
@@ -15,5 +59,10 @@ public class Fruit_Item : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+    }
+
+    private void OnValidate()
+    {
+        
     }
 }
